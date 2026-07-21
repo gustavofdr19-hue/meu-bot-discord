8ef375d3b181841a984760afafdc498f14cbc421
gustavofdr19-hue <gustavofdr19@gmail.com>
2026-07-21 20:19:48 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot online como: {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!oi'):
+        await message.channel.send('Olá! Estou te obedecendo direto do Render!')
+
+token = os.environ.get('DISCORD_TOKEN')
+client.run(token)
